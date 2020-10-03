28a62f05d0c345e8597bd4daf815b956727626df
itonorimitsu <g1644088@gmail.com>
2020-10-03 16:43:55 +0900

    comment about old code.

--- src/Main.java
+++ src/Main.java
@@ -39,8 +39,30 @@ public class Main {
 		// ここを全てListに置き換える
 		// String tempMonster[] = new String[22];
 		ArrayList<String> tempMonster = new tempMonster<String>();
-		// double tempMonsterRar[] = new double[22];
+		// double tempMonsterRare[] = new double[22];
 		ArrayList<Double> tempMonsterRare = new tempMonsterRare<Double>();
+		// tempMonster[0]="イガキン";	tempMonsterRare[0]=9;
+		// tempMonster[1]="ナマチュウ";	tempMonsterRare[1]=3;
+		// tempMonster[2]="イノウエン";	tempMonsterRare[2]=1;
+		// tempMonster[3]="リョージィ";	tempMonsterRare[3]=2;
+		// tempMonster[4]="アキモトン";	tempMonsterRare[4]=5;
+		// tempMonster[5]="ゴージマ";	tempMonsterRare[5]=4;
+		// tempMonster[6]="チュウデン";	tempMonsterRare[6]=6;
+		// tempMonster[7]="ヅカホン";	tempMonsterRare[7]=8;
+		// tempMonster[8]="ニシムラー";	tempMonsterRare[8]=7;
+		// tempMonster[9]="サコーデン";	tempMonsterRare[9]=2;
+		// tempMonster[10]="ウッチー";	tempMonsterRare[10]=5;
+		// tempMonster[11]="ハヤッシー";	tempMonsterRare[11]=4;
+		// tempMonster[12]="キーチー";	tempMonsterRare[12]=3;
+		// tempMonster[13]="リョクン";	tempMonsterRare[13]=1;
+		// tempMonster[14]="デコポン";	tempMonsterRare[14]=6;
+		// tempMonster[15]="カミサン";	tempMonsterRare[15]=5;
+		// tempMonster[16]="シスイ";	tempMonsterRare[16]=1;
+		// tempMonster[17]="ジョナ";	tempMonsterRare[17]=7;
+		// tempMonster[18]="ギダギダ";	tempMonsterRare[18]=2;
+		// tempMonster[19]="ミッツー";	tempMonsterRare[19]=8;
+		// tempMonster[20]="ゾエサン";	tempMonsterRare[20]=5;
+		// tempMonster[21]="キタバー";	tempMonsterRare[21]=3;
 
 		tempMonster.set(0, "イガキン");	tempMonsterRare.set(0, 9);
 		tempMonster.set(1, "ナマチュウ");	tempMonsterRare.set(1, 3);
